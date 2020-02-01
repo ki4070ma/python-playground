bce4f5e2e79266340e7ea87bf4f23ce8bdda9c3c
Atsushi Mori <ki4070ma@gmail.com>
2020-02-01 21:55:32 +0900

    [isort] tweak

--- isort_/sample.py
+++ isort_/sample.py
@@ -1,5 +1,6 @@
 
-from .tooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo_long_package.sooooooooooooooooooooooooooo_long_package.calc import sub
+from tooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo_long_package. \
+    sooooooooooooooooooooooooooo_long_package.calc import sub
 
 if __name__ == '__main__':
     print(sub(2, 1))
