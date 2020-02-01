203d9c30849fdccf96f0c7e9504105906a4b0e9d
Atsushi Mori <ki4070ma@gmail.com>
2020-02-01 21:40:27 +0900

    [isort] Add sample codes

---
+++ isort_/sample.py
@@ -0,0 +1,5 @@
+
+from .tooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo_long_package.sooooooooooooooooooooooooooo_long_package.calc import sub
+
+if __name__ == '__main__':
+    print(sub(2, 1))
